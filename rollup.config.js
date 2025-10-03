d5ea44eab0d99ff34c65f6e9d7e0420fa38ca13f
Cian Hughes <cian@counselhealth.com>
2025-10-03 12:21:27 +0100

    add json() to fix

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
 
 export default {
   input: 'node_modules/dd-trace/packages/dd-trace/src/appsec/iast/analyzers/template-injection-analyzer.js',
@@ -6,5 +7,5 @@ export default {
     dir: 'output',
     format: 'cjs'
   },
-  plugins: [commonjs()]
+  plugins: [commonjs(), json()]
 };
